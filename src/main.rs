b2f5ce283b7febddc29acf0ddc0402856b88e124
Manoj Singh Negi <justanothermanoj@gmail.com>
2024-04-21 14:46:22 +0530

    ✨ Added ability to add test cases

--- src/main.rs
+++ src/main.rs
@@ -3,88 +3,22 @@ mod types;
 mod interpreter;
 mod lexer;
 mod scope;
+mod runtime;
 
-use parser::Parser;
-use interpreter::Interpreter;
-use lexer::Lexer;
-use scope::Scope;
+use runtime::Runtime;
 
 fn main() {
     let code = r#"
-        let x = 1;
-        let y = 2;
-        let c = "People";
-        let i = 0;
-
-        {
-            let y = 8;
-            let x = 1;
-            log(x);
-
-            if (x == 1) {
-                let y = 2;
-                log(x + " hello");
-                log(y);
-            }
-
-            log(x + 1);
-            log(y);
-        }
-
+        let a = 10;
+        let b = 20;
+        
         function add(a, b) {
-            return a + " " + b + " " + c;
+            return a + b;
         }
 
-        function log_something (something) {
-            log(something);
-        }
-
-        log(x < y); => true
-        log(x > y); => false
-        log(x <= y); => true
-        log(x >= y); => false
-
-
+        return add(a, b);
     "#;
 
-    let mut lexer = Lexer::new(code);
-    let mut tokens = Vec::new();
-
-    println!("Lexer started... \n");
-
-    while let Some(token) = lexer.next_token() {
-        tokens.push(token);
-    }
-
-    let mut i = 0;
-
-    for token in &tokens {
-        println!("{:?} => {:?}", i, token);
-        i += 1;
-    }
-
-    println!("\nLexer completed... \n");
-
-    println!("Parsing started... \n");
-
-    let mut j = 0;
-
-    let mut parser = Parser::new(tokens);
-    let ast = parser.parse();
-
-    for stmt in &ast {
-        println!("{:?} => {:#?}", j, stmt);
-        j += 1;
-    }
-
-    println!("\nParsing completed... \n");
-
-    println!("Execution started... \n");
-
-    let scope = Scope::new(None);
-
-    let mut interpreter = Interpreter::new(Some(scope));
-    interpreter.eval(ast);
-
-    println!("\nExecution completed... \n");
+    let mut runtime = Runtime::new(code);
+    runtime.execute();
 }
---
+++ src/runtime/mod.rs
@@ -0,0 +1,66 @@
+use crate::scope::Scope;
+use crate::parser::Parser;
+use crate::interpreter::Interpreter;
+use crate::lexer::Lexer;
+use crate::types::{Token, Stmt, Value};
+
+pub struct Runtime<'a> {
+    tokens: Vec<Token>,
+    ast: Vec<Stmt>,
+    output: Value,
+    scope: Scope,
+    code: &'a str
+}
+
+impl<'a> Runtime<'a> {
+    pub fn new(code: &'a str) -> Self {
+        Runtime {
+            tokens: Vec::new(),
+            ast: Vec::new(),
+            output: Value::None,
+            scope: Scope::new(None),
+            code
+        }
+    }
+
+    pub fn execute (&mut self) -> Value {
+        let mut lexer = Lexer::new(&self.code);
+        println!("Lexer started... \n");
+
+        while let Some(token) = lexer.next_token() {
+            self.tokens.push(token);
+        }
+
+        let mut i = 0;
+
+        for token in &self.tokens {
+            println!("{:?} => {:?}", i, token);
+            i += 1;
+        }
+
+        println!("\nLexer completed... \n");
+
+        println!("Parsing started... \n");
+
+        let mut j = 0;
+
+        let mut parser = Parser::new(&self.tokens);
+        self.ast = parser.parse();
+
+        let local_ast = self.ast.clone();
+
+        for stmt in &local_ast {
+            println!("{:?} => {:#?}", j, stmt);
+            j += 1;
+        }
+
+        println!("\nParsing completed... \n");
+
+        println!("Execution started... \n");
+
+        let scope = Scope::new(None);
+
+        let mut interpreter = Interpreter::new(Some(scope));
+        interpreter.eval(local_ast)
+    }
+}

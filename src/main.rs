085b9a0dda094451f24f593fd5ba7d8209cac149
Manoj Singh Negi <justanothermanoj@gmail.com>
2024-05-01 21:48:03 +0530

    ✨ Add REPL functionality and improve code execution
    Simplify the main function and introduce a REPL to interact with the
    runtime. Update the `execute` method to parse AST before execution and
    return executed value. Remove unnecessary scope field from Runtime
    struct to avoid confusion.

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,19 @@ mod types;
 use runtime::Runtime;
 
 fn main() {
-    let code = r#"
-        function fib (n) {
-            if (n <= 1) {
-                return n;
-            }
+    // let code = r#"
+    //     function fib (n) {
+    //         if (n <= 1) {
+    //             return n;
+    //         }
 
-            return fib(n - 1);
-        }
+    //         return fib(n - 1);
+    //     }
 
-        log(fib(100))
-    "#;
+    //     log(fib(100))
+    // "#;
 
-    let mut runtime = Runtime::new(code);
-    runtime.execute();
+    let mut runtime = Runtime::new("");
+    runtime.repl();
+    // runtime.execute();
 }
